b0099251c122a5c4a077c8f9b03a13dae4f7db0c
aronvaupel <vaupelaron@gmail.com>
2021-08-10 18:23:17 +0200

    Add new route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
+
+app.get('/api/credentials', async (_req, res) => {
+  res.json(await readCredentials());
+});
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
