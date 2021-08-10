ffe07d0dacdf5f02360a10e99f15d8c24523afb5
aronvaupel <vaupelaron@gmail.com>
2021-08-10 14:04:58 +0200

    Create readPassword function

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
-console.log('hello server!');
+import { readFile } from 'fs/promises';
+
+async function readPasswords() {
+  const data = await readFile('src/db.json', 'utf-8');
+  console.log(data);
+}
+readPasswords();
