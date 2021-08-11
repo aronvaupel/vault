a8eec6461b0139c0deb134e807f8c6a7ba953fd1
aronvaupel <vaupelaron@gmail.com>
2021-08-11 11:39:22 +0200

    Add try/catch syntax

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,28 @@ const port = 3000;
 
 app.get('/api/credentials/:service', async (req, res) => {
   const { service } = req.params;
-  res.json(await getCredential(service));
+  try {
+    const credential = await getCredential(service);
+    res.status(200).json(credential);
+  } catch (error) {
+    res.status(404).send(`Could not find service: ${service}`);
+  }
 });
 
 app.get('/api/credentials', async (_req, res) => {
-  res.json(await readCredentials());
+  try {
+    res.status(200).json(await readCredentials());
+  } catch (error) {
+    response.status(500).send('Internal server error');
+  }
 });
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  try {
+    res.send('Hello World!');
+  } catch (error) {
+    response.status(404).send('Can´t connect to server!');
+  }
 });
 
 app.listen(port, () => {
