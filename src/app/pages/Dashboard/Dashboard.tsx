6f0fa68641e0c2bbf2c7f7f182a95dc26200cb22
aronvaupel <vaupelaron@gmail.com>
2021-08-19 16:36:52 +0200

    Add useEffect to read credentials

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Dashboard.module.css';
 
 export default function Dashboard(): JSX.Element {
+  const [credentials, setCredentials] = useState<Credential[]>([]);
+  useEffect(() => {
+    async function fetchCredentials() {
+      const response = await fetch('/api/credentials', {
+        headers: {
+          Authorization: 'Hund',
+        },
+      });
+      const credentials = await response.json();
+      setCredentials(credentials);
+    }
+    fetchCredentials();
+  }, []);
   return (
     <>
       <h1 className={styles.container}>Vault</h1>
       <p>My personal password manager</p>
       <input type="text" placeholder="Masterpassword" />
       <div>container for later</div>
+      {credentials?.forEach((credential) => console.log(credential))}
     </>
   );
 }
