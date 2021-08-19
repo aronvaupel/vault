4d10939b3d637dd58716e92e052a2e4a9e0ff85e
aronvaupel <vaupelaron@gmail.com>
2021-08-19 10:42:51 +0200

    Add aditiona path and nav

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 import Dashboard from './pages/Dashboard/Dashboard';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/services">Services</Link>
+        <Link to="/">Dashboard</Link>
+      </nav>
       <Switch>
-        <Route path="/">
-          <div>extra route</div>
+        <Route path="/services">
+          <div>Services</div>
         </Route>
         <Route path="/">
           <Dashboard />
