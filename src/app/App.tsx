4d9e48b706aaf5cca3c9523710c7aa7da5beb088
aronvaupel <vaupelaron@gmail.com>
2021-08-19 12:12:51 +0200

    Create dynamic route for password

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
 import Dashboard from './pages/Dashboard/Dashboard';
-
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Password from './pages/password/password';
 
 function App(): JSX.Element {
   return (
@@ -10,8 +9,12 @@ function App(): JSX.Element {
       <nav>
         <Link to="/services">Services</Link>
         <Link to="/">Dashboard</Link>
+        <Link to="/password/services">Passwords</Link>
       </nav>
       <Switch>
+        <Route path="/password/:service">
+          <Password />
+        </Route>
         <Route path="/services">
           <div>Services</div>
         </Route>
