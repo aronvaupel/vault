d66308b0409e979bd80a84d7d27973e93b8952ad
aronvaupel <vaupelaron@gmail.com>
2021-08-20 14:38:54 +0200

    Add new Paths for buttons and remove Nav

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Password from './pages/password/password';
+import AddPage from './pages/AddPage/AddPage';
+import SearchPage from './pages/SearchPage/SearchPage';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/services">Services</Link>
-        <Link to="/">Dashboard</Link>
-        <Link to="/password/services">Passwords</Link>
-      </nav>
       <Switch>
         <Route path="/password/:service">
           <Password />
         </Route>
-        <Route path="/services">
-          <div>Services</div>
+        <Route path="/add">
+          <AddPage />
         </Route>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/:credentialID/edit">x</Route>
+        <Route path="/:credentialID">x</Route>
         <Route path="/">
           <Dashboard />
         </Route>
